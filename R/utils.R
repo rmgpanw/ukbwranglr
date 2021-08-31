0d53ab5f6358faf9f4aaae1ef1ea8c008b99626a
rmgpanw <rmgpanw@ucl.ac.uk>
2021-08-31 16:13:26 +0100

    remove my_skim function

--- R/utils.R
+++ R/utils.R
@@ -4,47 +4,7 @@
 
 # Summarise ---------------------------------------------------------------
 
-#' Quickly summarise a dataframe
-#'
-#' A tidyverse-friendly summary function that summarises a dataframe by column type
-#'
-#' Works with \code{dplyr::group_by()} and the pipe. See the
-#' \code{\link[skimr]{skim}} documentation for more details. Adapts the
-#' \code{skimr::skim()} function to include proportion counts for factor
-#' variables
-#'
-#' @inheritParams skimr::skim
-#'
-#' @export
-my_skim <- skimr::skim_with(
-  # factor - a long anonymous function that converts a prop table to a single string
-  factor = skimr::sfl(pct = function(x) {
-    # make a prop table in %
-    pct_table <- prop.table(table(x)) * 100
 
-    # round % to 1dp
-    pct_table <- round(pct_table, 1)
-
-    # zip the table names and values together
-    combined_vector <- vector(mode = 'character', length = 0L)
-    for (i in 1:length(pct_table)) {
-      combined_vector <- c(combined_vector, paste0(names(pct_table)[i], ":"))
-      combined_vector <- c(combined_vector, paste0(as.character(pct_table)[i], "%,"))
-    }
-
-    # see result
-    combined_vector
-
-    # glue character vector into single string
-    combined_vector <- stringr::str_c(combined_vector, collapse = " ")
-
-    # return result
-    return(combined_vector)
-  }),
-
-  # logical - returns proportion = TRUE
-  logical = skimr::sfl(pct_TRUE = function(x) {sum(x == TRUE, na.rm=TRUE) / length(x) * 100})
-)
 
 # Mappings - data dictionary/codings --------------------------------------
 
