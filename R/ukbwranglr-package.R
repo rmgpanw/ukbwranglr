ca4e7541bcb1db052112b601495e665926b78fb9
rmgpanw <rmgpanw@ucl.ac.uk>
2022-05-06 15:21:44 +0100

    move dbplyr back to Imports in DESCRIPTION; update package documentaiton .R file

---
+++ R/ukbwranglr-package.R
@@ -0,0 +1,13 @@
+globalVariables(
+  c(".", ".data", ".SD", "new_col", "..selected_cols")
+)
+
+#' @keywords internal
+#' @importFrom dbplyr sql
+#' @importFrom magrittr "%>%"
+#' @importFrom data.table ":="
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
